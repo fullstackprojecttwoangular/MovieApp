90c8a45a637a289aa2baa2ce5edd34d630b4edcf
Terry <tdawgstudio@gmail.como>
2019-05-11 15:43:57 -0400

    cleaned up files missing npm files from last dev pull added back in

--- src/app/session/custom-interceptor.session.ts
+++ src/app/session/custom-interceptor.session.ts
@@ -15,7 +15,7 @@ export class CustomInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
         request = request.clone({
-            withCredentials: true
+           
         });
         return next.handle(request);
     }
