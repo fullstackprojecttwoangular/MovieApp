9e2904657081741217efcd08b53fe52052f9693a
matt <mattcowin82@gmail.com>
2019-05-13 05:23:50 -0400

    Switching to try something with the login logout

--- src/app/components/logout/logout.component.ts
+++ src/app/components/logout/logout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { Users } from 'src/app/models/users';
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
@@ -7,12 +8,14 @@ import {Router} from '@angular/router';
 })
 export class LogoutComponent implements OnInit {
 
+  
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   logout(){
+    localStorage.clear();
     this.router.navigate(['main']);
   }
 }
